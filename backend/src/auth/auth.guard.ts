10a4062c6868b8da8f155b025f66a206a529c757
Davi Oliveira <daviun.oliveira@gmail.com>
2025-12-09 01:37:47 -0300

    feat(backend): add nest guard to protect endpoints by requiring a valid jwt in the request

---
+++ backend/src/auth/auth.guard.ts
@@ -0,0 +1,39 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  // Verifica se o usuário está autenticado através do token JWT
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('Token de autenticação está ausente.');
+    }
+    try {
+      const payload: Record<string, unknown> =
+        await this.jwtService.verifyAsync(token, {
+          secret: process.env.JWT_SECRET,
+        });
+
+      request['userId'] = payload;
+    } catch {
+      throw new UnauthorizedException('Token de autenticação inválido.');
+    }
+    return true;
+  }
+
+  // Extrai o token do cabeçalho Authorization
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
