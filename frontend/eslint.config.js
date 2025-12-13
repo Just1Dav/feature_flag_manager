0b4f8dd431b8f3f7754a954e2ada43a5e4ad005f
Davi Oliveira <daviun.oliveira@gmail.com>
2025-12-13 11:45:34 -0300

    chore(frontend): add ui folder into eslint ignore

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,14 @@ import eslint from '@eslint/js';
 
 export default [
   {
-    ignores: ['eslint.config.js', 'dist/**', '.astro/**', 'node_modules/**', 'src/generated/**'],
+    ignores: [
+      'eslint.config.js',
+      'dist/**',
+      '.astro/**',
+      'node_modules/**',
+      'src/generated/**',
+      'src/components/ui/**',
+    ],
   },
   eslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
